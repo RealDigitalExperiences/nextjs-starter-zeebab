580a77ab3966c36e8ec79fd8edf44c52e204dfbf
Frikan Erwee <ferwee@gmail.com>
2023-02-22 13:13:39 +0200

    wip

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -13,9 +13,6 @@ const FeaturedProducts = () => {
           <span className="text-base-regular text-gray-600 mb-6">
             Latest products
           </span>
-          <p className="text-2xl-regular text-gray-900 max-w-lg mb-4">
-            Our newest styles are here to help you look your best.
-          </p>
           <UnderlineLink href="/store">Explore products</UnderlineLink>
         </div>
         <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-4 gap-y-8">
