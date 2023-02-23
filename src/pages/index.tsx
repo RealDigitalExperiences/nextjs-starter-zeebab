8573e7e7a47fdecf24b8036ab07ab9c9312ba709
Frikan Erwee <ferwee@gmail.com>
2023-02-23 20:49:38 +0200

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPageWithLayout = () => {
     <>
       <Head
         title="Home"
-        description="Shop all available models only at the Hennies. Worldwide Shipping. Secure Payment."
+        description="The Real Hennie's - Beste kuier in Suid Afrika!"
       />
       <Hero />
       <FeaturedProducts />
